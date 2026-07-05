7b4606f35a927c33cf07cd8cf0eb4e19ef7059d1
Lee wells <129164817+leewells2@users.noreply.github.com>
2026-07-05 01:22:16 +0100

    Update components.js

--- js/components.js
+++ js/components.js
@@ -2,10 +2,15 @@ async function loadComponent(id, file) {
 
     const response = await fetch(file);
 
+    if (!response.ok) {
+        console.error(`Couldn't load ${file}`);
+        return;
+    }
+
     document.getElementById(id).innerHTML =
         await response.text();
 
 }
 
-loadComponent("header", "components/header.html");
-loadComponent("footer", "components/footer.html");
+loadComponent("header", "./components/header.html");
+loadComponent("footer", "./components/footer.html");
